f6789481acbd84e8272d457a5dc509603e954e69
ubrnt <ubrnt.dev@gmail.com>
2026-06-30 02:07:28 +0100

    fix OrderServiceTest name

--- src/test/java/ru/yandex/practicum/mymarket/service/OrderServiceTest.java
+++ src/test/java/ru/yandex/practicum/mymarket/service/OrderServiceTest.java
@@ -26,7 +26,7 @@ import ru.yandex.practicum.mymarket.repository.projection.ItemDetailedRow;
 import ru.yandex.practicum.mymarket.repository.projection.OrderItemDetailedRow;
 
 @ExtendWith(MockitoExtension.class)
-class kOrderServiceTest {
+class OrderServiceTest {
 
     @Mock
     OrderRepository orderRepository;
